770d86375e4fbc597710a08c826d596b74f21f7c
Josias <ndjosiasaurel@gmail.com>
2025-05-25 13:52:56 +0100

    can download db

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
 from news import get_recent_week_entries, get_and_write_news
+import os
+
+SQLITE_DB_PATH = "./tops.db"
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -15,3 +18,12 @@ async def read_index(request: Request):
 async def health_check(request: Request):
     get_and_write_news()
     return {"status": "ok", "message": "News entries updated successfully"}
+
+
+@app.get("/download-db")
+def download_db():
+    return FileResponse(
+        path=SQLITE_DB_PATH,
+        media_type='application/octet-stream',
+        filename=os.path.basename(SQLITE_DB_PATH)
+    )
