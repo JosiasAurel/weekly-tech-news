d07862ccd03efe7681ea73e8050884b0ce249ae6
Josias <ndjosiasaurel@gmail.com>
2025-05-25 13:33:08 +0100

    add endpoint to update news database

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import uvicorn
-from news import get_recent_week_entries
+from news import get_recent_week_entries, get_and_write_news
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -11,6 +11,11 @@ templates = Jinja2Templates(directory="templates")
 async def read_index(request: Request):
     articles = get_recent_week_entries()
     return templates.TemplateResponse("index.html", {"request": request, "articles": articles})
+
+@app.get("/update-news")
+async def health_check(request: Request):
+    get_and_write_news()
+    return {"status": "ok", "message": "News entries updated successfully"}
 
 # Optional: run the server if executed directly
 if __name__ == "__main__":
